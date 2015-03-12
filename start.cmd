f9670b9863d806a0f31cc01d6dc3ebc0ca73df77
Nikita-kun <nibogd@users.noreply.github.com>
2015-03-12 20:34:31 +0300

    Create start.cmd

---
+++ start.cmd
@@ -0,0 +1,30 @@
+@echo off
+REM ingress-ice start script by Nikitakun (http://github.com/nibogd/ingress-ice)
+REM               SETTINGS 
+REM Set GOOGLELOGIN to your google login,
+REM PASSWORD to your password, AREA to yo
+REM ur link (read README.md or 
+REM http://github.com/nibogd/ingress-ice)
+REM , MINLEVEL to minimum portal level (s
+REM et to 1 to display all available port
+REM als), MAXLEVEL to maximum portal leve
+REM l (highest portal level, set to 8 to 
+REM display all), DELAY to delay between 
+REM capturing screenshots, FOLDER to fold
+REM er where to save (with \ on the end, 
+REM . means current folder)
+
+set GOOGLELOGIN=changeThisToYourGoogleLogin
+set PASSWORD=setThisToYourPassword
+set AREA=https://www.ingress.com/intel?ll=35.682398,139.693909&z=11
+set MINLEVEL=1
+set MAXLEVEL=8
+set DELAY=60
+set WIDTH=900
+set HEIGHT=500
+set FOLDER=.\
+
+rem DO NOT EDIT ANYTHING BELOW THIS LINE
+
+phantomjs.exe ice.js %GOOGLELOGIN% %PASSWORD% %AREA% %MINLEVEL% %MAXLEVEL% %DELAY% %WIDTH% %HEIGHT% %FOLDER%
+pause
