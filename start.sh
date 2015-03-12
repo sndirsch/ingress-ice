f3562cb35c2314427d5b49503785e2487a5ce0f1
Nikita-kun <nibogd@users.noreply.github.com>
2015-03-12 19:55:26 +0300

    Start Script
    Starter script for linux

---
+++ start.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# ingress-ice start script by Nikitakun (http://github.com/nibogd/ingress-ice
+#               SETTINGS          
+L=''            #google login or email
+P=''            #google password
+AREA='https://www.ingress.com/intel?ll=35.682398,139.693909&z=11' #link to your location
+MINLEVEL='1'    #minimal portal level, set to 1 to display all available portals
+MAXLEVEL='8'    #highest portal level, set to 8 to display all
+V='60'          #Delay between capturing screenshots in seconds
+WIDTH='900'     #Picture width
+HEIGHT='500'    #Picture height
+FOLDER='./'     #Folder where to save screenshots with \ (or /) in the end. '.' means current folder. 
+#DO NOT EDIT ANYTHING BELOW THIS LINE
+ARGS="$L $P $AREA $MINLEVEL $MAXLEVEL $V $WIDTH $HEIGHT $FOLDER"
+for arg
+do
+    if [ "$arg" = --help ]
+    then
+       echo "Please visit http://github.com/nibogd/ingress-ice for help"
+    fi
+done
+
+exec ./phantomjs ice.js $ARGS
