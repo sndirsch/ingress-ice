401681c3032db31737ee3a80f7433ec1a2d91361
c2nprds <c2nprds@gmail.com>
2015-08-26 20:38:26 +0900

    introduce amazon s3 file upload method

--- ice.js
+++ ice.js
@@ -123,7 +123,8 @@ var loginTimeout = 10 * 1000;
 * twostep auth trigger
 */
 var twostep      = 0;
-var page         = require('webpage').create();
+var webpage      = require('webpage');
+var page         = webpage.create();
 page.onConsoleMessage = function () {};
 page.onError  = function () {};
 
@@ -251,6 +252,54 @@ function s() {
   announce('Screen saved');
   page.render(folder + 'ice-' + getDateTime(1) + '.png');
 }
+
+/**
+* Upload AWS S3
+* @see http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html
+* @see ALC https://docs.aws.amazon.com/AmazonS3/latest/dev/acl-overview.html#canned-acl
+* @param {String} key - AWS S3 accessKeyId
+* @param {String} secret - AWS S3 secretKeyId
+* @param {String} bucket - AWS S3 bucket name
+* @param {String} alc - AWS S3 access controll list
+* @param {String} path - Screenshot filepath
+* @param {Boolean} remove - delete current file
+*/
+function uploadS3(key, secret, bucket, alc, path, remove) {
+  var s3 = webpage.create();
+  s3.onError = function(msg, trace) {};
+  s3.content = '<html><body><input id="file-chooser" name="file-chooser" type="file"></body></html>';
+
+  s3.onCallback = function(data) {
+    if (data.status == 200) {
+      announce('Successfully! file upload for Amazon S3');
+      if (remove) fs.remove(path);
+    } else {
+      announce('Failure! file upload for Amazon S3');
+    }
+    s3.close();
+  };
+
+  s3.includeJs('http://sdk.amazonaws.com/js/aws-sdk-2.1.34.min.js', function() {
+    s3.evaluate(function(key, secret, bucket, alc) {
+      var fileChooser = document.querySelector('#file-chooser');
+      fileChooser.addEventListener('change', function() {
+        var f = fileChooser.files[0];
+        if (f) {
+          AWS.config.update({accessKeyId: key, secretAccessKey: secret});
+          var params = {Key: f.name, ContentType: f.type, Body: f};
+          var b = new AWS.S3({params: {Bucket: bucket, ACL: alc}});
+          b.putObject(params, function (err, data) {
+            window.callPhantom({ status: (err ? 500 : 200), data: data });
+          });
+        } else {
+          window.callPhantom({ status: 400 });
+        }
+      }, false);
+    }, key, secret, bucket, alc);
+
+    s3.uploadFile("input[name=file-chooser]", path);
+  });
+};
 
 /**
 * Quit if an error occured
