1e8814be55957776c6fed3c9d398c70e2d14fee8
c2nprds <c2nprds@gmail.com>
2015-08-26 19:10:01 +0900

    cosme

--- ice.js
+++ ice.js
@@ -215,30 +215,30 @@ function setMinMax(min, max) {
     page.sendEvent('click', rect.left + rect.width / 2, rect.top + rect.height / 2);
     window.setTimeout(function() {
       var rect1 = page.evaluate(function(min) {
-	return document.querySelector('#level_low' + min).getBoundingClientRect();
+        return document.querySelector('#level_low' + min).getBoundingClientRect();
       }, min);
       page.sendEvent('click', rect1.left + rect1.width / 2, rect1.top + rect1.height / 2);
       if (max === 8) {
-	page.evaluate(function () {
-	  document.querySelector('#filters_container').style.display = 'none';
-	});
+        page.evaluate(function () {
+          document.querySelector('#filters_container').style.display = 'none';
+        });
       }
     }, 2000);
   }
   if (max < 8) {
     window.setTimeout(function() {
       var rect2 = page.evaluate(function() {
-	return document.querySelectorAll('.level_notch.selected')[1].getBoundingClientRect();
+        return document.querySelectorAll('.level_notch.selected')[1].getBoundingClientRect();
       });
       page.sendEvent('click', rect2.left + rect2.width / 2, rect2.top + rect2.height / 2);
       window.setTimeout(function() {
-	var rect3 = page.evaluate(function(min) {
-	  return document.querySelector('#level_high' + min).getBoundingClientRect();
-	}, max);
-	page.sendEvent('click', rect3.left + rect3.width / 2, rect3.top + rect3.height / 2);
-	page.evaluate(function () {
-	  document.querySelector('#filters_container').style.display = 'none';
-	});
+        var rect3 = page.evaluate(function(min) {
+          return document.querySelector('#level_high' + min).getBoundingClientRect();
+        }, max);
+        page.sendEvent('click', rect3.left + rect3.width / 2, rect3.top + rect3.height / 2);
+        page.evaluate(function () {
+          document.querySelector('#filters_container').style.display = 'none';
+        });
       }, 2000);
     }, 4000);
   }
@@ -347,8 +347,8 @@ function checkLogin() {
     if (page.url.substring(0,40) === 'https://appengine.google.com/_ah/loginfo') {
       announce('Accepting appEngine request...');
       page.evaluate(function () {
-	document.getElementById('persist_checkbox').checked = true;
-	document.getElementsByTagName('form').submit();
+        document.getElementById('persist_checkbox').checked = true;
+        document.getElementsByTagName('form').submit();
       });
     }
 
@@ -359,11 +359,11 @@ function checkLogin() {
 
     if (twostep) {
       page.evaluate(function (code) {
-	document.getElementById('totpPin').value = code;
+        document.getElementById('totpPin').value = code;
       }, twostep);
       page.evaluate(function () {
-	document.getElementById('submit').click();
-	document.getElementById('challenge').submit();
+        document.getElementById('submit').click();
+        document.getElementById('challenge').submit();
       });
     }
 }
@@ -378,25 +378,25 @@ function afterPlainLogin() {
     checkLogin();
     window.setTimeout(function () {
       page.open(area, function () {
-	storeCookies();
-	if (iitc) {
-	  addIitc();
-	}
-	setTimeout(function () {
-	  announce('Will start screenshooting in ' + v/1000 + ' seconds...');
-	  if (((minlevel > 1)||(maxlevel < 8)) && !iitc) {
-	    setMinMax(minlevel, maxlevel);
-	  } else if (!iitc) {
-	    page.evaluate(function () {
-	      document.querySelector("#filters_container").style.display= 'none';
-	    });
-	  }
-	  hideDebris(iitc);
-	  prepare(iitc, width, height);
-	  announce('The first screenshot may not contain all portals, it is intended for you to check framing.');
-	  main();
-	  setInterval(main, v);
-	}, loginTimeout);
+        storeCookies();
+        if (iitc) {
+          addIitc();
+        }
+        setTimeout(function () {
+          announce('Will start screenshooting in ' + v/1000 + ' seconds...');
+          if (((minlevel > 1)||(maxlevel < 8)) && !iitc) {
+            setMinMax(minlevel, maxlevel);
+          } else if (!iitc) {
+            page.evaluate(function () {
+              document.querySelector("#filters_container").style.display= 'none';
+            });
+          }
+          hideDebris(iitc);
+          prepare(iitc, width, height);
+          announce('The first screenshot may not contain all portals, it is intended for you to check framing.');
+          main();
+          setInterval(main, v);
+        }, loginTimeout);
       });
     }, loginTimeout);
   }, loginTimeout);
@@ -411,10 +411,10 @@ function afterCookieLogin() {
     if(!isSignedIn()) {
       removeCookieFile();
       if(l && p) {
-	firePlainLogin();
-	return;
+        firePlainLogin();
+        return;
       } else {
-	quit('User not logged in');
+        quit('User not logged in');
       }
     }
     if (iitc) {
@@ -423,11 +423,11 @@ function afterCookieLogin() {
     setTimeout(function () {
       announce('Will start screenshooting in ' + v/1000 + ' seconds...');
       if (((minlevel > 1)||(maxlevel < 8)) && !iitc) {
-	setMinMax(minlevel, maxlevel, iitc);
+        setMinMax(minlevel, maxlevel, iitc);
       } else if (!iitc) {
-	page.evaluate(function () {
-	  document.querySelector("#filters_container").style.display= 'none';
-	});
+        page.evaluate(function () {
+          document.querySelector("#filters_container").style.display= 'none';
+        });
       }
       hideDebris(iitc);
       prepare(iitc, width, height);
@@ -477,31 +477,31 @@ function count() {
 function hideDebris(iitcz) {
   if (!iitcz) {
     page.evaluate(function () {
-      if (document.querySelector('#comm'))           {document.querySelector('#comm').style.display = 'none';}
-      if (document.querySelector('#player_stats'))   {document.querySelector('#player_stats').style.display = 'none';}
-      if (document.querySelector('#game_stats'))     {document.querySelector('#game_stats').style.display = 'none';}
-      if (document.querySelector('#geotools'))       {document.querySelector('#geotools').style.display = 'none';}
-      if (document.querySelector('#header'))         {document.querySelector('#header').style.display = 'none';}
-      if (document.querySelector('#snapcontrol'))    {document.querySelector('#snapcontrol').style.display = 'none';}
-      if (document.querySelectorAll('.img_snap')[0]) {document.querySelectorAll('.img_snap')[0].style.display = 'none';}
+      if (document.querySelector('#comm'))             {document.querySelector('#comm').style.display = 'none';}
+      if (document.querySelector('#player_stats'))     {document.querySelector('#player_stats').style.display = 'none';}
+      if (document.querySelector('#game_stats'))       {document.querySelector('#game_stats').style.display = 'none';}
+      if (document.querySelector('#geotools'))         {document.querySelector('#geotools').style.display = 'none';}
+      if (document.querySelector('#header'))           {document.querySelector('#header').style.display = 'none';}
+      if (document.querySelector('#snapcontrol'))      {document.querySelector('#snapcontrol').style.display = 'none';}
+      if (document.querySelectorAll('.img_snap')[0])   {document.querySelectorAll('.img_snap')[0].style.display = 'none';}
       if (document.querySelector('#display_msg_text')) {document.querySelector('#display_msg_text').style.display = 'none';}
     });
     page.evaluate(function () {
       var hide = document.querySelectorAll('.gmnoprint');
       for (var index = 0; index < hide.length; ++index) {
-	hide[index].style.display = 'none';
+        hide[index].style.display = 'none';
       }
     });
   } else {
     window.setTimeout(function () {
       page.evaluate(function () {
-	if (document.querySelector('#chat'))                            {document.querySelector('#chat').style.display = 'none';}
-	if (document.querySelector('#chatcontrols'))                    {document.querySelector('#chatcontrols').style.display = 'none';}
-	if (document.querySelector('#chatinput'))                       {document.querySelector('#chatinput').style.display = 'none';}
-	if (document.querySelector('#updatestatus'))                    {document.querySelector('#updatestatus').style.display = 'none';}
-	if (document.querySelector('#sidebartoggle'))                   {document.querySelector('#sidebartoggle').style.display = 'none';}
-	if (document.querySelector('#scrollwrapper'))                   {document.querySelector('#scrollwrapper').style.display = 'none';}
-	if (document.querySelector('.leaflet-control-container')) {document.querySelector('.leaflet-control-container').style.display = 'none';}
+        if (document.querySelector('#chat'))                      {document.querySelector('#chat').style.display = 'none';}
+        if (document.querySelector('#chatcontrols'))              {document.querySelector('#chatcontrols').style.display = 'none';}
+        if (document.querySelector('#chatinput'))                 {document.querySelector('#chatinput').style.display = 'none';}
+        if (document.querySelector('#updatestatus'))              {document.querySelector('#updatestatus').style.display = 'none';}
+        if (document.querySelector('#sidebartoggle'))             {document.querySelector('#sidebartoggle').style.display = 'none';}
+        if (document.querySelector('#scrollwrapper'))             {document.querySelector('#scrollwrapper').style.display = 'none';}
+        if (document.querySelector('.leaflet-control-container')) {document.querySelector('.leaflet-control-container').style.display = 'none';}
       });
     }, 2000);
   }
@@ -602,16 +602,16 @@ function prepare(iitcz, widthz, heightz) {
   } else {
     window.setTimeout(function () {
       page.evaluate(function (w, h) {
-	var water = document.createElement('p');
-	water.id='viewport-ice';
-	water.style.position = 'absolute';
-	water.style.top = '0';
-	water.style.marginTop = '0';
-	water.style.paddingTop = '0';
-	water.style.left = '0';
-	water.style.width = w;
-	water.style.height = h;
-	document.querySelectorAll('body')[0].appendChild(water);
+        var water = document.createElement('p');
+        water.id='viewport-ice';
+        water.style.position = 'absolute';
+        water.style.top = '0';
+        water.style.marginTop = '0';
+        water.style.paddingTop = '0';
+        water.style.left = '0';
+        water.style.width = w;
+        water.style.height = h;
+        document.querySelectorAll('body')[0].appendChild(water);
       }, widthz, heightz);
       var selector = "#viewport-ice";
       setElementBounds(selector);
@@ -627,8 +627,8 @@ function setElementBounds(selector) {
   page.clipRect = page.evaluate(function(selector) {
     var clipRect = document.querySelector(selector).getBoundingClientRect();
     return {
-      top:     clipRect.top,
-      left:     clipRect.left,
+      top:    clipRect.top,
+      left:   clipRect.left,
       width:  clipRect.width,
       height: clipRect.height
     };
@@ -659,8 +659,8 @@ function main() {
   if (timestamp) {
     page.evaluate(function () {
       if (document.getElementById('watermark-ice')) {
-	var oldStamp = document.getElementById('watermark-ice');
-	oldStamp.parentNode.removeChild(oldStamp);
+        var oldStamp = document.getElementById('watermark-ice');
+        oldStamp.parentNode.removeChild(oldStamp);
       }
     });
   }
@@ -694,9 +694,9 @@ function cookiesFileExists() {
       var line = stream.readLine();
       var res = line.split('=');
       if(res[0] === 'SACSID') {
-	cookieSACSID = res[1];
+        cookieSACSID = res[1];
       } else if(res[0] === 'csrftoken') {
-	cookieCSRF = res[1];
+        cookieCSRF = res[1];
       }
     }
     stream.close();
