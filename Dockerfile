1c1214309bdd18ba3819e22248e4ab3a05f0f2a9
Nikita-kun <linuxoid.x64@gmail.com>
2016-02-19 18:43:11 +0400

    Fix Docker support (after arch updated), implement #71

--- Dockerfile
+++ Dockerfile
@@ -3,12 +3,12 @@ FROM base/archlinux
 # Based on Dockerfile by pheanex
 # Usage:
 # $ docker build -t ingress-ice .
-# $ docker run -d -v /path/to/.ingress-ice.conf:/.ingress-ice.conf -v /path/to/screenshots/:/screenshots ingress-ice
+# $ docker run -v /path/to/.ingress-ice.conf:/.ingress-ice.conf -v /path/to/screenshots/:/screenshots ingress-ice
 MAINTAINER Nikitakun <linuxoid.x64@gmail.com>
 
 RUN pacman-key --populate archlinux
 RUN pacman-key --refresh-keys
-RUN pacman -Sy phantomjs ttf-droid --noconfirm
+RUN pacman -Sy phantomjs ttf-droid libstdc++5 gperf icu libjpeg-turbo libpng python2 ruby gcc gcc-libs freetype2 texinfo zlib gdbm expat libffi gmp libyaml curl pcre perl --noconfirm
 ADD . /ingress-ice/
 RUN chmod +x /ingress-ice/docker-ingress-ice.sh
 RUN mkdir /screenshots
--- docker-ingress-ice.sh
+++ docker-ingress-ice.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
 config_file="/.ingress-ice.conf"
-screenshot_folder="/screenshots/"
+screenshot_folder="/screenshots"
 if ! [[ -r "$config_file" ]]
 then
-	echo "The configuration file does not exists. Please create it first and then specify it as a docker volume!" >&2
-	exit 1
+        if ! [[ -r "$screenshot_folder$config_file" ]]
+        then
+                echo "The configuration file does not exists. Please create it first and then specify it as a docker volume or 
+put it into screenshots directory as $config_file" >&2
+                exit 1
+        else
+                config_file="$screenshot_folder$config_file"
+        fi
 fi
 
 umask 0000
 
-cd /screenshots
-phantomjs /ingress-ice/ice/ice.js /.ingress-ice.conf > ingress-ice.log
+cd $screenshot_folder
+phantomjs "/ingress-ice/ice/ice.js" "$config_file" > ingress-ice.log
