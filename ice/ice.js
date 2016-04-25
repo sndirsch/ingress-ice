ad2e9a945946b8bc783621a6411d9836ef92c320
Nikita-kun <linuxoid.x64@gmail.com>
2016-04-25 19:58:56 +0400

    Version bump to 4.4.0

--- ice/ice.js
+++ ice/ice.js
@@ -1,7 +1,7 @@
 /**
  * @file Ingress-ICE, the main script
  * @author Nikitakun (https://github.com/nibogd)
- * @version 4.3.0
+ * @version 4.4.0
  * @license MIT
  * @see {@link https://github.com/nibogd/ingress-ice|GitHub }
  * @see {@link https://ingress.netlify.com/|Website }
@@ -16,7 +16,7 @@
 var system    = require('system');
 var args      = system.args;
 var fs        = require('fs');
-var version   = '4.3.0';
+var version   = '4.4.0';
 var filename  = 'ice.js';
 var iceFolder = args[0].substring(0, args[0].length - filename.length) + 'modules/';
 var iceModules= fs.list(iceFolder);
